c7c1924ade69ae8bb6f1d43fcd546872e4156e30
tahaimran804 <itaha3753@gmail.com>
2025-10-07 20:01:49 +0500

    myUpdates

--- Components/Home_Components/HeroSection.jsx
+++ Components/Home_Components/HeroSection.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Container from '../Container'
 import ProductSlider from './ProductSlider'
 import style from '@/app/Style/Hero.module.css'
@@ -12,7 +13,9 @@ const HeroSection = () => {
           <h1 className='text-gray-700 text-5xl'>One Stop Solution <span className='text-4xl font-bold text-indigo-600'>E-Store</span></h1>
           <p className='text-2xl'>Discover The Latest Headphone, Earphone, Mobiles, Tablet, Smart Watches, Etc</p>
           <p className='text-2xl'>Best Deal Just For You</p>
-          <button className='flex items-center gap-3 mt-8 bg-transparent py-2 px-6 rounded-full border hover:text-indigo-600'>Show Now <span className='text-indigo-500'><FaArrowRight /></span></button>
+          <Link href='/store'>
+            <button className='flex items-center gap-3 mt-8 bg-transparent py-2 px-6 rounded-full border hover:text-indigo-600'>Show Now <span className='text-indigo-500'><FaArrowRight /></span></button>
+          </Link>
         </section>
         <ProductSlider />
       </div>
